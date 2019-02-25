ee918a7b3a8a07632f0abdd18a6c74dcbf782a34
Ludwing Pérez <ludwing@ludwing-idei>
2019-02-25 11:47:47 -0600

    Comunicación con la API de last.fm para obtener los nombres de los artistas top en un país.

---
+++ platzimusic/src/api/config.js
@@ -0,0 +1,10 @@
+export default {
+    appName: 'lwmusic',
+    apiKey: '3553227e1b293aceb9c18e832f17b7fe',
+    secret: 'd882fc55c2219e8aa4b0dbdc30226379',
+    registeredTo: 'ludwingguate'
+}
+// Application name    lwmusic
+// API key     3553227e1b293aceb9c18e832f17b7fe
+// Shared secret   d882fc55c2219e8aa4b0dbdc30226379
+// Registered to   ludwingguate
